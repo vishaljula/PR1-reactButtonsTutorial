98074d03e67af2d1ddcf6ccce0f5b90b690557b8
vishaljula <vishaljula@users.noreply.github.com>
2017-04-12 23:30:26 -0400

    Added class and function components for buttons app

--- simple-buttons-app/src/App.js
+++ simple-buttons-app/src/App.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Result from './components/functions/Result';
+import Button from './components/classes/Button';
 
-class App extends Component {
+class App extends React.Component {
+  state = {counter: 0};
+
+  incrementCounter = (incrementBy) => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + incrementBy
+    }));
+  };
+
   render() {
     return (
       <div>
-        <h1>Buttons App</h1>
+        <Button incrementBy={1} onClickFunction={this.incrementCounter}/>
+        <Button incrementBy={5} onClickFunction={this.incrementCounter}/>
+        <Button incrementBy={10} onClickFunction={this.incrementCounter}/>
+        <Button incrementBy={15} onClickFunction={this.incrementCounter}/>
+        <Result counter={this.state.counter}/>
       </div>
     );
   }
